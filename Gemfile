b3151669b177c14ff4d1e879bb6360c9b5e2622e
Kristian Golemi <krigol0014@gmail.com>
2022-02-09 20:44:30 +0200

    Added json web token class and configured methods to handle possible exceptions it might throw

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "puma", "~> 5.0"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem 'bcrypt', '~> 3.1.7'
+gem 'jwt'
 
 group :development, :test do
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,24 @@
+class JsonWebToken
+    # secret to encode and decode token
+    # every rails application has a unique secret key
+    HMAC_SECRET = Rails.application.secrets.secret_key_base
+    
+    # method responsible for creating tokens based on user id and expiration period
+    def self.encode(payload, exp = 24.hours.from_now)
+      # set expiry to 24 hours from creation time
+      payload[:exp] = exp.to_i
+      # sign token with application secret
+      JWT.encode(payload, HMAC_SECRET)
+    end
+  
+    # method which accepts a token and attempts to decode it using the same secret used in the encoding process
+    def self.decode(token)
+      # get payload; first index in decoded Array
+      body = JWT.decode(token, HMAC_SECRET)[0]
+      HashWithIndifferentAccess.new body
+      # rescue from all decode errors
+    rescue JWT::DecodeError => e
+      # raise custom error to be handled by custom handler
+      raise ExceptionHandler::InvalidToken, e.message
+    end
+  end
