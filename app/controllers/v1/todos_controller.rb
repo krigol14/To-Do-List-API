ca916632ea3e4a22323ca1d515196ea3b814ab15
mfkres <0713549@gmail.com>
2022-02-10 22:06:29 +0200

    Added API Versioning

--- app/controllers/v1/todos_controller.rb
+++ app/controllers/v1/todos_controller.rb
@@ -1,4 +1,5 @@
-class TodosController < ApplicationController
+module V1
+  class TodosController < ApplicationController
     before_action :set_todo, only: [:show, :update, :destroy]
   
     # GET /todos
@@ -43,3 +44,4 @@ class TodosController < ApplicationController
       @todo = Todo.find(params[:id])
     end
   end
+end
